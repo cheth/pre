caf9b12a5af88bd47bcc4be52e3b4036b58f5b38
Cheth Rowe <cheth@cheth.com>
2016-10-04 09:19:28 -0700

    Move markdown preetifier to extensions directory.

--- extensions/pre.config.php
+++ extensions/pre.config.php
@@ -9,9 +9,17 @@
 * 1.0.0 2016-Sep-17 (cheth) initial implementation
 *******************************/
 
+//--define interfaces for optional features-----
+$pre->requireIfExists($extensions_path . 'pre.db.interface.php'); // database interface
+$pre->requireIfExists($extensions_path . 'pre.md.interface.php'); // Markdown interface
+
 //--optional database class-----
-require_once ($this->include_path . 'pre.db.implementation.php');
-require_once ($this->include_path . 'MySQL_PDO_Model.php');
-$this->setDatabaseHelper(new Database_Helper);
+$pre->requireIfExists($extensions_path . 'pre.db.implementation.php'); // between interface and PDO helper
+$pre->requireIfExists($extensions_path . 'MySQL_PDO_Model.php'); // PDO helper
+$pre->setDatabaseHelper(new Database_Helper);
+
+//--optional Markdown class-----
+$pre->requireIfExists($extensions_path . 'pre.prettifier.php');
+$pre->setMarkdownHelper(new Prettification_Helper);
 
 // eof: pre.config.php
