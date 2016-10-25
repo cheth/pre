bcd483619f3098f0b272f578dd58aeb9ef7cb10f
Cheth Rowe <cheth@cheth.com>
2016-10-25 11:12:03 -0700

    Markdown extension supports unordered item lists.

--- extensions/pre.prettifier.php
+++ extensions/pre.prettifier.php
@@ -20,6 +20,7 @@ class Prettification_Helper implements Markdown_Interface {
     private $div_verb = '~~~';
     private $is_pre = FALSE;
     private $is_div = FALSE;
+    private $is_ul = FALSE;
     private $previous_line = '';
     private $previous_character = '';
     private $current_character = '';
@@ -166,353 +167,12 @@ class Prettification_Helper implements Markdown_Interface {
             }
             continue;
 
-            //--end paragraph----------
-            if ($current_character === "\n" && $previous_character === "\n" && $is_paragraph) {
-                $formatted_text .= "</p>\n";
-                $is_paragraph = FALSE;
-                continue;
-            }
-
-            //--end <h1> to <h6>----------
-            if ($current_character === "\n" && $is_header) {
-                $formatted_text .= "</h$h_counter>\n";
-                $h_counter = 0;
-                $is_header = FALSE;
-                continue;
-            }
-
-            //--begin <h1> to <h6>----------
-            if ($current_character === "#" && ($previous_character === "\n" || $i===0)) {
-                $h_counter = 1;
-                continue;
-            }
-
-            //--count <h2> to <h6>----------
-            if ($current_character === "#" && $previous_character === "#" && $h_counter && !$is_header) {
-                $h_counter++;
-                if ($h_counter <= 6) {
-                    continue;
-                } 
-                // if more than 6 then abandon <h*> attempt
-                $formatted_text .= '#######'; // 7
-                $h_counter = 0;
-            }
-
-            //--start <h*> tag----------
-            if ($current_character !== "#" && $previous_character === "#" && $h_counter && !$is_header) {
-                $formatted_text .= "<h$h_counter>";
-                $is_header = TRUE;
-            }
-
-            //--end triple backtick pre----------------
-            if ($current_character === "\n" && $previous_character === "`" && $is_pre) {
-                $is_pre = FALSE;
-                $formatted_text .= "</pre>\n";
-                continue;
-            };     
-
-            if ($current_character === "`" && $is_pre) {
-                continue; // expecting three backticks; but any number will end pre section
-            };     
-
-            //--begin triple backtick pre----------------
-            if ($current_character === "`" && ($previous_character === "\n" || $i===0) && !$is_pre) {
-                $backtick_counter = 1;
-                continue;
-            }
-
-            if ($current_character === "`" && $previous_character === "`" && $backtick_counter && !$is_pre) {
-                $backtick_counter++;
-                if ($backtick_counter <= 3) {
-                    continue;
-                } 
-                // if more than 3 then abandon <pre> attempt
-                $formatted_text .= '````'; // 4
-                $backtick_counter = 0;
-            }
-
-            //--start backtick <pre> tag----------
-            if ($current_character !== "`" && $previous_character === "`" && $backtick_counter===3 && !$is_pre) {
-                $formatted_text .= "<pre>\n";
-                $is_pre = TRUE;
-                $backtick_counter = 0;
-                if ($current_character === "\n") {
-                    continue;
-                }
-            }
-
-            //--begin pre----------------
-            if ($current_character === "`" && $previous_character === "`" && !$is_pre) {
-                $is_pre = TRUE;
-                $formatted_text .= "<pre>\n";
-                continue;
-            };     
-
-            if ($current_character === "\n" && $previous_character === '~' && !$is_code) {
-                continue;
-            };     
-
-            //--end pre----------------
-            if ($current_character === '~' && $previous_character === '~' && $is_pre) {
-                $is_pre = FALSE;
-                $formatted_text .= "</pre>\n";
-                continue;
-            };     
-
-            if ($current_character === "\n" && $previous_character === '~' && !$is_pre) {
-                continue;
-            };     
-
-            //--begin code----------------
-            if ($current_character !== '~' && $is_pre && !$is_code) {
-                $is_code = TRUE;
-                $formatted_text .= "<code>";
-            };     
-
-            //--end code----------------
-            if ($current_character === "\n" && $previous_character === "\n" && $is_code) {
-                $is_code = FALSE;
-                $formatted_text .= "&nbsp;</code>\n";
-                continue;
-            };
-
-            if ($current_character === "\n" && $is_code) {
-                $is_code = FALSE;
-                $formatted_text .= "</code>\n";
-                continue;
-            };
-
-            //--begin raw html----------------
-            if ($current_character === '%' && $previous_character === '%' && !$is_raw_html) {
-                $is_raw_html = TRUE;
-                continue;
-            };     
-
-            //--end raw html----------------
-            if ($current_character === '%' && $previous_character === '%' && $is_raw_html) {
-                continue;
-            };     
-
-            if ($current_character === "\n" && $previous_character === '%' && $is_raw_html) {
-                $is_raw_html = FALSE;
-                continue;
-            };     
-
-            //--begin pre heading----------------
-            if ($current_character === '{' && $previous_character === '{' && !$is_pre_heading) {
-                $is_pre_heading = TRUE;
-                $formatted_text .= '<div class="pre_heading">';
-                continue;
-            };     
-
-            //--end pre heading----------------
-            if ($current_character === '}' && $previous_character === '}' && $is_pre_heading) {
-                $is_pre_heading = FALSE;
-                $formatted_text .= "</div>\n";
-                continue;
-            };     
-
-            if ($current_character === "\n" && $previous_character === '}' && !$is_pre_heading) {
-                continue;
-            };     
-
-            //--begin sub-heading----------------
-            if ($current_character === '^' && $previous_character === '^' && !$is_sub_heading) {
-                $is_sub_heading = TRUE;
-                $formatted_text .= '<div class="sub_heading">';
-                continue;
-            };     
-
-            //--end sub-heading----------------
-            if ($current_character === '^' && $previous_character === '^' && $is_sub_heading) {
-                $is_sub_heading = FALSE;
-                $formatted_text .= "</div>\n";
-                continue;
-            };     
-
-            if ($current_character === "\n" && $previous_character === '^' && !$is_sub_heading) {
-                continue;
-            };     
-
-            //--begin pull quote----------------
-            if ($current_character === '|' && $previous_character === '|' && !$is_pull_quote) {
-                $is_pull_quote = TRUE;
-                $formatted_text .= '<div class="pull_quote">';
-                continue;
-            };     
-
-            //--end pull quote----------------
-            if ($current_character === '|' && $previous_character === '|' && $is_pull_quote) {
-                $is_pull_quote = FALSE;
-                $formatted_text .= "</div>\n";
-                continue;
-            };     
-
-            if ($current_character === "\n" && $previous_character === '|' && !$is_pull_quote) {
-                continue;
-            };     
-
-            //--begin code fragment----------------
-            if ($current_character === '$' && $previous_character === '$' && !$is_code_fragment) {
-                $is_code_fragment = TRUE;
-                $formatted_text .= '<span class="code_fragment">';
-                continue;
-            };     
-
-            //--end code fragment----------------
-            if ($current_character === '$' && $previous_character === '$' && $is_code_fragment) {
-                $formatted_text .= "</span>\n";
-                $is_code_fragment = FALSE;
-                $current_character = ''; // hack to prevent restart of code_fragment
-                continue;
-            };     
-
-            //--power character preservation----------
-            if ($current_character !== '`' && $previous_character === '`' && !$is_pre) {
-                $formatted_text .= '`';
-            };
-                
-            if ($current_character !== '~' && $previous_character === '~' && !$is_pre) {
-                $formatted_text .= '~';
-            };
-                
-            if ($current_character !== '{' && $previous_character === '{' && !$is_pre_heading) {
-                $formatted_text .= '{';
-            };
-
-            if ($current_character !== '}' && $previous_character === '}' && !$is_pre_heading) {
-                $formatted_text .= '}';
-            };
-
-            if ($current_character !== '^' && $previous_character === '^' && !$is_sub_heading) {
-                $formatted_text .= '^';
-            };
-                
-            if ($current_character !== '|' && $previous_character === '|' && !$is_pull_quote) {
-                $formatted_text .= '|';
-            };
-                
-            if ($current_character !== '%' && $previous_character === '%' && !$is_raw_html) {
-                $formatted_text .= '%';
-            };
-                
-            if ($current_character !== '$' && $previous_character === '$' && !$is_code_fragment) {
-                $formatted_text .= '$';
-            };
-
-            //--delay processing of power characters----------
-            if ($current_character === '`') {
-                continue;
-            };
-
-            if ($current_character === '~') {
-                continue;
-            };
-
-            if ($current_character === '{') {
-                continue;
-            };
-
-            if ($current_character === '}') {
-                continue;
-            };
-
-            if ($current_character === "^") {
-                continue;
-            };
-
-            if ($current_character === "|") {
-                continue;
-            };
-
-            if ($current_character === "%") {
-                continue;
-            };
-
-            if ($current_character === "$") {
-                continue;
-            };
-
-            //--single newlines disappear; but leave at least a space----------
-            if ($current_character === "\n") {
-                if (strpos("\n ",substr($formatted_text,-1)) === FALSE) {
-                    $formatted_text .= ' ';
-                }    
-                continue;
-            };
-
-            //--start paragraph----------
-            if (!$is_pre && !$is_paragraph && !$is_pre_heading && !$is_sub_heading && !$is_pull_quote && !$h_counter) {
-                $is_paragraph = TRUE;
-                $formatted_text .= "<p>";
-            };
-
-            //--handle quotations--------
-            if ($is_paragraph && !$is_raw_html && $current_character === '"') {
-                if ($is_quoting) {
-                    //$formatted_text .= '&rdquo;'; // close quote
-                    $formatted_text .= '</q>'; // close quote
-                    $is_quoting = FALSE;
-                } else {
-                    //$formatted_text .= '&ldquo;'; // open quote
-                    $formatted_text .= '<q>'; // open quote
-                    $is_quoting = TRUE;
-                };
-                continue;
-            };
-
-            if ($is_paragraph && $current_character === "'") {
-                    $formatted_text .= '&apos;'; // single apostrophe
-                    continue;
-            };    
-
-            //--handle em dash--------
-            if ($is_paragraph && $current_character === '-' && $previous_character !== '-') {
-                continue;
-            };
-
-            if ($is_paragraph && $current_character === "-" && $previous_character === "-") {
-                $formatted_text .= '&mdash;'; // emit em dash (and done with current item)
-                $was_em_dash = TRUE; 
-                continue;
-            };    
-
-            if ($is_paragraph && $current_character !== "-" && $previous_character === "-" && !$was_em_dash) {
-                $formatted_text .= '-'; // emit hyphen (and whatever is actually in queue)
-            };    
-
-            if ($is_paragraph && $current_character !== "-" && $previous_character === "-" && $was_em_dash) {
-                $was_em_dash = FALSE;
-            };    
-
-            //--accept character----------
-            $formatted_text .= $current_character;
         };
 
         $this->formatted_text = trim($this->formatted_text);
         return ($this->formatted_text);
     }
 
-/*
-            if ($this->isPowerCharacter($current_character, $previous_character, $current_line)) {
-                if ($this->isCompleteVerb($current_character, $previous_character, $current_line)) {
-                    if ($this->isPairedVerbClosing) {
-                        $this->processStackedVerbs();
-                        $this->processCurrentVerb();
-                    }
-                    if ($this->isPairedVerbOpening) {
-                        $this->stackVerb();
-                    };    
-                    if (!$this->isPairedVerb) {
-                        $this->processVerb($current_character, $previous_character, $current_line);
-                    };
-                    continue;    
-                }
-            } else {
-                $this->pushStringStack($current_character);
-            }
-*/
-            
     /**********************************************
     * helperHref()
     *------------------
@@ -533,7 +193,7 @@ class Prettification_Helper implements Markdown_Interface {
     *------------------
     * Purpose: push formatted text to output
     *------------------
-    * params: <string> text to add to output
+    * params: <string> text to add to output (can arrive as multiple lines)
     *------------------
     * returns: <Boolean> TRUE if successful
     ***********************************************/
@@ -546,6 +206,24 @@ class Prettification_Helper implements Markdown_Interface {
         if ($this->is_pre) {
             $this->formatted_text .= "<code>$line</code>\n";
             return TRUE;
+        }
+
+        //--end li items (+ in column 1)---------
+        if ($this->is_ul && substr($line,0,1) !== '+') {
+            $this->formatted_text .= "</ul>\n";
+            $this->is_ul = FALSE;
+        }    
+
+        //--expand li items (+ in column 1)
+        if (substr($line,0,1) === '+') {
+            if ($this->is_ul === FALSE) {
+                $this->formatted_text .= "<ul>\n";
+                $this->is_ul = TRUE;
+            };
+            $line = trim(substr($line,1)); // bypass initial +
+            $line = str_replace("\n+","</li>\n<li>",$line); // multiple lines fed here at once
+            $this->formatted_text .= "<li>$line</li>\n";
+            return TRUE; 
         }
 
         //--headings-------------
